b90e866936bccd8365c7f4fe52c4cb596fc14e7f
Vineel Sadineni <vin.anddev@gmail.com>
2026-07-18 13:38:04 -0700

    Implement multiple linear regression for CO2 emissions
    Added multiple linear regression model implementation with training and testing datasets. Included visualizations and exercises for CO2 emissions analysis based on engine size and fuel consumption.

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -114,3 +114,137 @@ max	3.57	4.50
 As you can see, a standardized variable has zero mean and a standard deviation of one.
 '''
 
+'''
+Create train and test datasets
+ - Randomly split your data into train and test sets, using 80% of the dataset for training and reserving the remaining 20% for testing.
+Build a multiple linear regression model
+ - Multiple and simple linear regression models can be implemented with exactly the same scikit-learn tools.
+'''
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+X_train, X_test, y_train, y_test = train_test_split(X_std, y, test_size = 0.20, random_state = 1)
+
+lr = LinearRegression()
+lr.fit( X_train  , y_train) 
+
+coef_ = lr.coef_
+intercept_ = lr.intercept_
+
+# the coefficiant and intercept
+print("the Coefficients: ", lr.coef_) # there are two coeffients as we are taking EngineSize, FUELCONSUMPTION_COMB_MPg
+print("Intercept:", lr.intercept_)
+
+'''
+Coefficients:  [[ 25.27339614 -37.4381472 ]]
+Intercept:  [256.29072488]
+
+The Coefficients and Intercept parameters define the best-fit hyperplane to the data. Since there are only two variables, hence two parameters, the hyperplane is a plane.
+But this best-fit plane will look different in the original, unstandardized feature space.
+
+You can transform your model's parameters back to the original space prior to standardization as follows. This gives you a proper sense of what they mean in terms of your original input features.
+Without these adjustments, the model's outputs would be tied to an abstract,
+transformed space that doesn’t align with the actual independent variables and the real-world problem you’re solving.
+'''
+# Get the standard scaler's mean and standard deviation parameters
+means_ = std_scaler.mean_
+std_devs_ = np.sqrt(std_scaler.var_)
+
+# The least squares parameters can be calculated relative to the original, unstandardized feature space as:
+coef_original = coef_ / std_devs_
+intercept_original = intercept_ - np.sum((means_ * coef_) / std_devs_)
+
+print ('Coefficients: ', coef_original)
+print ('Intercept: ', intercept_original)
+'''
+Coefficients:  [[18.51930924 -4.88859447]]
+Intercept:  [323.76587705]
+'''
+
+plt.scatter(X_train[:,0], y_train,  color='blue')
+plt.plot(X_train[:,0], coef_[0,0] * X_train[:,0] + intercept_[0], '-r') # coef_[0,0] from [[18.51930924 -4.88859447]] is 18.51930924
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(X_train[:,1], y_train,  color='blue')
+plt.plot(X_train[:,1], coef_[0,1] * X_train[:,1] + intercept_[0], '-r') # coef_[0,1] from  [[18.51930924 -4.88859447]] is -4.88859447
+plt.xlabel("FUELCONSUMPTION_COMB_MPG")
+plt.ylabel("Emission")
+plt.show()
+
+'''
+Exercise 1
+Determine and print the parameters for the best-fit linear regression line for CO2 emission with respect to engine size.
+'''
+from sklearn.linear_model import LinearRegression
+
+X_train_1 = X_train[:,0]
+
+lr = LinearRegression()
+lr.fit(X_train_1.reshape(-1,1), y_train)
+
+coef_1 =  lr.coef_
+intercept_1 = lr.intercept_
+
+print ('Coefficients: ',coef_1)
+print ('Intercept: ',intercept_1)
+'''
+Coefficients:  [[55.78187587]]
+Intercept:  [256.66618567]
+'''
+
+'''
+Exercise 2
+Produce a scatterplot of CO2 emission against ENGINESIZE and include the best-fit regression line to the training data.
+'''
+X_train_1 = X_train[:,0] # all rows and first column ie, Engine size of training data set
+
+plt.scatter(X_train_1 , y_train , color ="blue")
+plt.plot(X_train_1 , coef_1[0] * X_train_1 + intercept_1 , color = 'red') # y = mx + c
+plt.xlabel("EngineSize")
+plt.ylabel("CO2Emissions")
+plt.show()
+
+'''
+Exercise 3
+Generate the same scatterplot and best-fit regression line, but now base the result on the test data set. Consider how the test result compares to the training result.
+'''
+X_test_1 = X_test[:,0] # all rows and first column ie, Engine size of test data set
+
+plt.scatter(X_test_1, y_test,  color='blue')
+plt.plot(X_test_1, coef_1[0] * X_test_1 + intercept_1, '-r')
+plt.xlabel("Test: Engine size")
+plt.ylabel("Test : CO2 Emission test")
+plt.show()
+
+'''
+Exercise 4
+Repeat the same modeling but use FUELCONSUMPTION_COMB_MPG as the independent variable instead. Display the model coefficients including the intercept.
+'''
+X_train_2 = X_train[:,1] # selecting the FUELCONSUMPTION_COMB_MPG
+
+lr = LinearRegression()
+lr.fit(X_train_2.reshape(-1,1), y_train)
+
+coef_fuel = lr.coef_
+intercept_fuel = lr.intercept_
+
+print("the coefficient of  FUELCONSUMPTION_COMB_MPG ", coef_fuel )
+print("the intercept of  FUELCONSUMPTION_COMB_MPG ", intercept_fuel )
+'''
+the coefficient of  FUELCONSUMPTION_COMB_MPG  [[-57.43890997]]
+the intercept of  FUELCONSUMPTION_COMB_MPG  [256.29512547]
+'''
+
+'''
+Exercise 5
+Generate a scatter plot showing the results as before on the test data. Consider well the model fits, and what you might be able to do to improve it.
+'''
+
+X_test_2 = X_test[:,1] # selecting the FUELCONSUMPTION_COMB_MPG from X_test dataset
+plt.scatter(X_test_2, y_test ,  color='blue')
+plt.plot(X_test_2, coef_fuel[0] * X_test_2 + intercept_fuel , '-r')
+plt.xlabel("Test: FUELCONSUMPTION_COMB_MPG")
+plt.ylabel("Test: CO2 Emission")
+plt.show()
